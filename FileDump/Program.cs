cd8c3a17ee5a229fe8f1bd13cc07a7816ac123f3
OrigamiTech <origamitech@gmail.com>
2010-12-08 19:20:44 +0000

    added 4bpp mode, with a flip option

--- FileDump/Program.cs
+++ FileDump/Program.cs
@@ -11,18 +11,42 @@ namespace FileDump
     {
         static void Main(string[] args)
         {
+            int width = 32;
+            bool
+                binary = false,
+                argb = false,
+                fbpp = false,
+                fbppf = false;
             Console.Write("Image Width: ");
-            int width = int.Parse(Console.ReadLine());
+            string strWidth = Console.ReadLine().Trim().ToLower();
+            if (strWidth == "auto")
+                ;//width = square root, or closest.
+            width = int.Parse(strWidth);
             Console.Write("Binary mode? ");
-            bool binary = Console.ReadLine().Trim().ToLower()[0] == 'y';
+            binary = Console.ReadLine().Trim().ToLower()[0] == 'y';
+            if (!binary)
+            {
+                Console.Write("ARGB color mode? ");
+                argb = Console.ReadLine().Trim().ToLower()[0] == 'y';
+            }
+            if (!binary&&!argb)
+            {
+                Console.Write("4bpp mode? ");
+                fbpp = Console.ReadLine().Trim().ToLower()[0] == 'y';
+            }
+            if (fbpp)
+            {
+                Console.Write("4bpp flip? ");
+                fbppf = Console.ReadLine().Trim().ToLower()[0] == 'y';
+            }
             foreach (string path in args)
             {
                 Bitmap b = new Bitmap(1, 1);
                 byte[] allbytes = File.ReadAllBytes(path);
-                b = new Bitmap(width, (int)Math.Ceiling((double)(allbytes.Length * (binary ? 8 : 1)) / width));
-                for (int i = 0; i < allbytes.Length * (binary ? 8 : 1); i++)
-                    b.SetPixel(i % (int)width, i / (int)width, binary ? (((allbytes[i / 8] >> (7 - (i % 8))) & 0x01) != 0 ? Color.Black : Color.White) : Color.FromArgb(allbytes[i], allbytes[i], allbytes[i]));
-                b.Save(path + "." + width.ToString("0") + (binary ? "b" : "") + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                b = new Bitmap(width, (int)Math.Ceiling((double)(allbytes.Length * (binary ? 8 : (argb ? 0.25 : (fbpp ? 2 : 1)))) / width));
+                for (int i = 0; i < allbytes.Length * (binary ? 8 : (argb ? 0.25 : (fbpp ? 2 : 1))); i++)
+                    b.SetPixel(i % (int)width, i / (int)width, binary ? (((allbytes[i / 8] >> (7 - (i % 8))) & 0x01) != 0 ? Color.Black : Color.White) : (argb ? Color.FromArgb(allbytes[i * 4], allbytes[i * 4 + 1], allbytes[i * 4 + 2], allbytes[i * 4 + 3]) : (fbpp ? Color.FromArgb((allbytes[i / 2] & ((i % 2 == 0) ^ fbppf ? 0xF0 : 0x0F)) << ((i % 2 == 0) ^ fbppf ? 0 : 4), (allbytes[i / 2] & ((i % 2 == 0) ^ fbppf ? 0xF0 : 0x0F)) << ((i % 2 == 0) ^ fbppf ? 0 : 4), (allbytes[i / 2] & ((i % 2 == 0) ^ fbppf ? 0xF0 : 0x0F)) << ((i % 2 == 0) ^ fbppf ? 0 : 4)) : Color.FromArgb(allbytes[i], allbytes[i], allbytes[i]))));
+                b.Save(path + "." + width.ToString("0") + (binary ? "b" : (argb ? "a" : (fbpp? "f" : ""))) + ".png", System.Drawing.Imaging.ImageFormat.Png);
                 b.Dispose();
             }
         }
